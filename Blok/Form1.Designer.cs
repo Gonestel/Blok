b5d00953b5a654200feadc1e651ad3a17b2a3702
Gonestel <www.ikfgbr@gmail.com>
2020-12-22 02:47:35 +0300

    Добавьте файлы проекта.

---
+++ Blok/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Blok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Кнопка = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Кнопка
+            // 
+            this.Кнопка.Location = new System.Drawing.Point(241, 157);
+            this.Кнопка.Name = "Кнопка";
+            this.Кнопка.Size = new System.Drawing.Size(75, 23);
+            this.Кнопка.TabIndex = 0;
+            this.Кнопка.Text = "Кнопка";
+            this.Кнопка.UseVisualStyleBackColor = true;
+            this.Кнопка.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(69, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 355);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Кнопка);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Кнопка;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Blok/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blok
+{
+    public partial class Form1 : Form
+    {
+        public int x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool Try()
+        {
+            try
+            {
+                x = Convert.ToInt32(textBox1.Text);
+                return false;
+            }
+            catch
+            {
+                return true;
+            }   
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Try() == false)
+            {
+                textBox2.Text = "Good";
+            }
+            else
+            {
+                textBox2.Text = "Error";
+            }
+        }
+    }
+}
